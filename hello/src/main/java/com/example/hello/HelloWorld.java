75a44fdc04fab93b4d67d784d16a442dc471ce89
HoHyeon Kim <khh5762@naver.com>
2023-05-30 03:28:42 +0900

    commit

--- hello/src/main/java/com/example/hello/HelloWorld.java
+++ hello/src/main/java/com/example/hello/HelloWorld.java
@@ -1,12 +1,13 @@
 package com.example.hello;
 
-import java.time.LocalTime;
+
+import org.joda.time.LocalTime;
 
 public class HelloWorld {
 
     public static void main(String[] args) {
-//        LocalTime currentTime = new LocalTime(12,15,15,10);
-//        System.out.println("The current local time is: " + currentTime);
+        LocalTime currentTime = new LocalTime();
+        System.out.println("The current local time is: " + currentTime);
 
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
--- vaadin/frontend/generated/jar-resources/vaadin-dev-tools/shim.ts
+++ vaadin/frontend/generated/jar-resources/vaadin-dev-tools/shim.ts
@@ -1,5 +1,5 @@
-import { css, html, LitElement } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import {css, html, LitElement} from 'lit';
+import {customElement} from 'lit/decorators.js';
 
 /**
  * Blocks interaction with all elements on the page. Forwards mouse and key events as custom shim-* events.
