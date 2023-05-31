e90ddf9d96b5f8bee28af8596e52641339a7c707
HoHyeon Kim <khh5762@naver.com>
2023-05-31 14:07:19 +0900

    Unversioned Files

---
+++ hello_maven/src/main/java/com/example/hello_maven/Greeter.java
@@ -0,0 +1,7 @@
+package com.example.hello_maven;
+
+public class Greeter {
+    public String sayHello() {
+        return "Hello world!";
+    }
+}
---
+++ jms_hello/src/test/java/com/example/jms_hello/JmsHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jms_hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsHelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
