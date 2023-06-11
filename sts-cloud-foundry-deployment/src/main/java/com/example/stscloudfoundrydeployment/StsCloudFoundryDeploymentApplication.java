7554d00fa89bddea21534514c342df6d1e0479e8
HoHyeon Kim <khh5762@naver.com>
2023-06-11 18:51:27 +0900

    sts 안함

---
+++ sts-cloud-foundry-deployment/src/main/java/com/example/stscloudfoundrydeployment/StsCloudFoundryDeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.example.stscloudfoundrydeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsCloudFoundryDeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsCloudFoundryDeploymentApplication.class, args);
+	}
+
+}
